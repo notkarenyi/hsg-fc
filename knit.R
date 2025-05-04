8d254cde38b467dee9f12d8f59076d0bde0eadf4
Karen <notkarenyi@gmail.com>
2025-05-04 18:18:44 -0500

    centralize script running in 1 place
    standardized data/template names

---
+++ knit.R
@@ -0,0 +1,20 @@
+quarter = "Fall 2024"
+
+for (format in c("pdf","html")) {
+  
+  fname <- paste0(
+    "Harris Student Government Finance Committee Transparency Report ", 
+    quarter
+  )
+  
+  rmarkdown::render(
+    paste0(quarter,".Rmd"),
+    encoding = encoding,
+    output_file = fname,
+    params = list(quarter),
+    output_format = paste0(format, "_document")
+  )
+  
+}
+
+file.remove(paste0(fname,".log"))

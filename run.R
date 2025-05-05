19f988fbd4b50e497360f9aa1f29b0c1a73fb32c
Karen <notkarenyi@gmail.com>
2025-05-05 12:49:06 -0500

    need named param

--- run.R
+++ run.R
@@ -62,7 +62,7 @@ for (format in c("pdf", "html")) {
 
   rmarkdown::render(
     paste0(report_quarter, ".Rmd"),
-    params = list(report_quarter),
+    params = list('quarter'=report_quarter),
     output_file = fp,
     output_format = paste0(format, "_document")
   )

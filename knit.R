08dd0c52d2c6940dd038455009774756cf262fff
Karen <notkarenyi@gmail.com>
2025-05-05 11:07:52 -0500

    run clean data from overall script

--- knit.R
+++ knit.R
@@ -1,16 +1,19 @@
-quarter <- "Winter 2025"
+report_quarter <- "Winter 2025"
+current_quarter <- "Spring 2025"
+
+source('clean_data.R')
 
 for (format in c("pdf", "html")) {
   fname <- paste0(
     "Harris Student Government Finance Committee Transparency Report ",
-    quarter
+    report_quarter
   )
 
   rmarkdown::render(
-    paste0(quarter, ".Rmd"),
+    paste0(report_quarter, ".Rmd"),
     encoding = encoding,
     output_file = fname,
-    params = list(quarter),
+    params = list(report_quarter),
     output_format = paste0(format, "_document")
   )
 }
